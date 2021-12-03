822a72d15c219fb002d9f1107264f2d3bb5d6cfb
MattPereira <matthewdavidpereira@gmail.com>
2021-12-03 12:32:20 -0800

    convert into express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/admissions', (req, res) => {
+    res.render('admissions')
+})
+
+app.get('/academics', (req, res) => {
+    res.render('academics')
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 300')
+})
