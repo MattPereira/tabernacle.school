ac4acb5465c5881260240a5aab6b24c2fa157a2b
Matthew Pereira <matthewdavidpereira@gmail.com>
2021-12-18 12:46:45 -0800

    reorganize images folder

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const homeRoutes = require('./routes/home');
 const familyRoutes = require('./routes/family');
 const facultyRoutes = require('./routes/faculty');
 
-
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', homeRoutes)
 app.use('/family', familyRoutes)
