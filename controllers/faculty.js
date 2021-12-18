84969449b0f49b6524156c66e6bca5080cebe1ff
Matthew Pereira <matthewdavidpereira@gmail.com>
2021-12-17 16:10:32 -0800

    set up faculty routes and controllers

---
+++ controllers/faculty.js
@@ -0,0 +1,175 @@
+//TK & K
+module.exports.charinaMoore = (req, res) => {
+    res.render('faculty/charinaMoore')
+}
+
+module.exports.sylviaBorde = (req, res) => {
+    res.render('faculty/sylviaBorde')
+}
+
+module.exports.bettyMason = (req, res) => {
+    res.render('faculty/bettyMason')
+}
+
+module.exports.ceciliaPrior = (req, res) => {
+    res.render('faculty/ceciliaPrior')
+}
+
+module.exports.linaFoltz = (req, res) => {
+    res.render('faculty/linaFoltz')
+}
+
+//1ST GRADE
+module.exports.alexisJue = (req, res) => {
+    res.render('faculty/alexisJue')
+}
+
+module.exports.emilyScouten = (req, res) => {
+    res.render('faculty/emilyScouten')
+}
+
+module.exports.terraJean = (req, res) => {
+    res.render('faculty/terraJean')
+}
+
+
+//2ND GRADE
+module.exports.tracyHerrera = (req, res) => {
+    res.render('faculty/tracyHerrera')
+}
+
+module.exports.ashleyLecair = (req, res) => {
+    res.render('faculty/ashleyLecair')
+}
+
+module.exports.heatherCochnauer = (req, res) => {
+    res.render('faculty/heatherCochnauer')
+}
+
+module.exports.rachelleLongtin = (req, res) => {
+    res.render('faculty/rachelleLongtin')
+}
+
+//3RD GRADE
+module.exports.pamelaLucido = (req, res) => {
+    res.render('faculty/pamelaLucido')
+}
+
+module.exports.noriSwanson = (req, res) => {
+    res.render('faculty/noriSwanson')
+}
+
+module.exports.haleyMax = (req, res) => {
+    res.render('faculty/haleyMax')
+}
+
+
+//4TH GRADE
+module.exports.stephanieKent = (req, res) => {
+    res.render('faculty/stephanieKent')
+}
+
+module.exports.jasonStone = (req, res) => {
+    res.render('faculty/jasonStone')
+}
+
+//5TH GRADE
+module.exports.brendaChapman = (req, res) => {
+    res.render('faculty/brendaChapman')
+}
+
+module.exports.aureliusMorgan = (req, res) => {
+    res.render('faculty/aureliusMorgan')
+}
+
+//JUNIOR HIGH
+module.exports.steveSouza = (req, res) => {
+    res.render('faculty/steveSouza')
+}
+
+module.exports.andyPrior = (req, res) => {
+    res.render('faculty/andyPrior')
+}
+
+module.exports.toddKeough = (req, res) => {
+    res.render('faculty/toddKeough')
+}
+
+module.exports.tirzahBurke = (req, res) => {
+    res.render('faculty/tirzahBurke')
+}
+
+module.exports.joeyPrior = (req, res) => {
+    res.render('faculty/joeyPrior')
+}
+
+//PHYSICAL EDUCATION
+module.exports.daranFraser = (req, res) => {
+    res.render('faculty/daranFraser')
+}
+
+module.exports.alexanderBuckley = (req, res) => {
+    res.render('faculty/alexanderBuckley')
+}
+
+module.exports.coryBrady = (req, res) => {
+    res.render('faculty/coryBrady')
+}
+
+module.exports.amieRobison = (req, res) => {
+    res.render('faculty/amieRobison')
+}
+
+//ELECTIVES 
+module.exports.judyBurmeister = (req, res) => {
+    res.render('faculty/judyBurmeister')
+}
+
+module.exports.rebeccaKelly = (req, res) => {
+    res.render('faculty/rebeccaKelly')
+}
+
+module.exports.kimberlynSchmitz = (req, res) => {
+    res.render('faculty/kimberlynSchmitz')
+}
+
+module.exports.kristenMcDermott = (req, res) => {
+    res.render('faculty/kristenMcDermott')
+}
+
+module.exports.margaretTrice = (req, res) => {
+    res.render('faculty/margaretTrice')
+}
+
+//ADMINISTRATION
+module.exports.vernTaylor = (req, res) => {
+    res.render('faculty/vernTaylor')
+}
+
+module.exports.theresaKukla = (req, res) => {
+    res.render('faculty/theresaKukla')
+}
+
+module.exports.davePereira = (req, res) => {
+    res.render('faculty/davePereira')
+}
+
+module.exports.rebecaShreve = (req, res) => {
+    res.render('faculty/rebecaShreve')
+}
+
+module.exports.pattiChan = (req, res) => {
+    res.render('faculty/pattiChan')
+}
+
+module.exports.lisaMazzoncini = (req, res) => {
+    res.render('faculty/lisaMazzoncini')
+}
+
+module.exports.lauriePease = (req, res) => {
+    res.render('faculty/lauriePease')
+}
+
+module.exports.cathyColomb = (req, res) => {
+    res.render('faculty/cathyColomb')
+}
--- index.js
+++ index.js
@@ -2,9 +2,14 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-
 const express = require('express');
 const path = require('path')
+
+const homeRoutes = require('./routes/home');
+const familyRoutes = require('./routes/family');
+const facultyRoutes = require('./routes/faculty');
+
+
 
 const app = express();
 
@@ -13,6 +18,10 @@ app.set('views', path.join(__dirname, '/views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+// app.use('/', homeRoutes)
+// app.use('/familyDashboard', familyRoutes)
+app.use('/faculty', facultyRoutes)
 
 
 // MAIN PAGES
@@ -112,176 +121,6 @@ app.get('/familyDashboard/forms/techAgreement', (req, res) => {
 
 app.get('/familyDashboard/forms/bicycleAgree', (req, res) => {
     res.render('familyDashboard/forms/bicycleAgree')
-})
-
-
-
-
-
-// TEACHER PROFILE PAGES
-
-app.get('/facultyProfiles/charinaMoore', (req, res) => {
-    res.render('facultyProfiles/charinaMoore')
-})
-
-app.get('/facultyProfiles/sylviaBorde', (req, res) => {
-    res.render('facultyProfiles/sylviaBorde')
-})
-
-app.get('/facultyProfiles/bettyMason', (req, res) => {
-    res.render('facultyProfiles/bettyMason')
-})
-
-app.get('/facultyProfiles/ceciliaPrior', (req, res) => {
-    res.render('facultyProfiles/ceciliaPrior')
-})
-
-app.get('/facultyProfiles/linaFoltz', (req, res) => {
-    res.render('facultyProfiles/linaFoltz')
-})
-
-app.get('/facultyProfiles/alexisJue', (req, res) => {
-    res.render('facultyProfiles/alexisJue')
-})
-
-app.get('/facultyProfiles/emilyScouten', (req, res) => {
-    res.render('facultyProfiles/emilyScouten')
-})
-
-app.get('/facultyProfiles/terraJean', (req, res) => {
-    res.render('facultyProfiles/terraJean')
-})
-
-app.get('/facultyProfiles/tracyHerrera', (req, res) => {
-    res.render('facultyProfiles/tracyHerrera')
-})
-
-app.get('/facultyProfiles/ashleyLecair', (req, res) => {
-    res.render('facultyProfiles/ashleyLecair')
-})
-
-app.get('/facultyProfiles/heatherCochnauer', (req, res) => {
-    res.render('facultyProfiles/heatherCochnauer')
-})
-
-app.get('/facultyProfiles/rachelleLongtin', (req, res) => {
-    res.render('facultyProfiles/rachelleLongtin')
-})
-
-app.get('/facultyProfiles/pamelaLucido', (req, res) => {
-    res.render('facultyProfiles/pamelaLucido')
-})
-
-app.get('/facultyProfiles/noriSwanson', (req, res) => {
-    res.render('facultyProfiles/noriSwanson')
-})
-
-app.get('/facultyProfiles/haleyMax', (req, res) => {
-    res.render('facultyProfiles/haleyMax')
-})
-
-app.get('/facultyProfiles/stephanieKent', (req, res) => {
-    res.render('facultyProfiles/stephanieKent')
-})
-
-app.get('/facultyProfiles/jasonStone', (req, res) => {
-    res.render('facultyProfiles/jasonStone')
-})
-
-app.get('/facultyProfiles/brendaChapman', (req, res) => {
-    res.render('facultyProfiles/brendaChapman')
-})
-
-app.get('/facultyProfiles/aureliusMorgan', (req, res) => {
-    res.render('facultyProfiles/aureliusMorgan')
-})
-
-app.get('/facultyProfiles/steveSouza', (req, res) => {
-    res.render('facultyProfiles/steveSouza')
-})
-
-app.get('/facultyProfiles/andyPrior', (req, res) => {
-    res.render('facultyProfiles/andyPrior')
-})
-
-app.get('/facultyProfiles/toddKeough', (req, res) => {
-    res.render('facultyProfiles/toddKeough')
-})
-
-app.get('/facultyProfiles/tirzahBurke', (req, res) => {
-    res.render('facultyProfiles/tirzahBurke')
-})
-
-app.get('/facultyProfiles/joeyPrior', (req, res) => {
-    res.render('facultyProfiles/joeyPrior')
-})
-
-app.get('/facultyProfiles/daranFraser', (req, res) => {
-    res.render('facultyProfiles/daranFraser')
-})
-
-app.get('/facultyProfiles/alexanderBuckley', (req, res) => {
-    res.render('facultyProfiles/alexanderBuckley')
-})
-
-app.get('/facultyProfiles/coryBrady', (req, res) => {
-    res.render('facultyProfiles/coryBrady')
-})
-
-app.get('/facultyProfiles/amieRobison', (req, res) => {
-    res.render('facultyProfiles/amieRobison')
-})
-
-app.get('/facultyProfiles/judyBurmeister', (req, res) => {
-    res.render('facultyProfiles/judyBurmeister')
-})
-
-app.get('/facultyProfiles/rebeccaKelly', (req, res) => {
-    res.render('facultyProfiles/rebeccaKelly')
-})
-
-app.get('/facultyProfiles/kimberlynSchmitz', (req, res) => {
-    res.render('facultyProfiles/kimberlynSchmitz')
-})
-
-app.get('/facultyProfiles/kristenMcDermott', (req, res) => {
-    res.render('facultyProfiles/kristenMcDermott')
-})
-
-app.get('/facultyProfiles/margaretTrice', (req, res) => {
-    res.render('facultyProfiles/margaretTrice')
-})
-
-app.get('/facultyProfiles/vernTaylor', (req, res) => {
-    res.render('facultyProfiles/vernTaylor')
-})
-
-app.get('/facultyProfiles/theresaKukla', (req, res) => {
-    res.render('facultyProfiles/theresaKukla')
-})
-
-app.get('/facultyProfiles/davePereira', (req, res) => {
-    res.render('facultyProfiles/davePereira')
-})
-
-app.get('/facultyProfiles/rebecaShreve', (req, res) => {
-    res.render('facultyProfiles/rebecaShreve')
-})
-
-app.get('/facultyProfiles/pattiChan', (req, res) => {
-    res.render('facultyProfiles/pattiChan')
-})
-
-app.get('/facultyProfiles/lisaMazzoncini', (req, res) => {
-    res.render('facultyProfiles/lisaMazzoncini')
-})
-
-app.get('/facultyProfiles/lauriePease', (req, res) => {
-    res.render('facultyProfiles/lauriePease')
-})
-
-app.get('/facultyProfiles/cathyColomb', (req, res) => {
-    res.render('facultyProfiles/cathyColomb')
 })
 
 
