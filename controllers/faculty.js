4d35a59d20e11c34b4c22b1c30e2cecefe8e1a46
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-02-03 16:04:46 -0800

    trade jason stone for eric Isaacson

--- controllers/faculty.js
+++ controllers/faculty.js
@@ -1,175 +1,173 @@
 //TK & K
 module.exports.charinaMoore = (req, res) => {
-    res.render('faculty/charinaMoore')
-}
+  res.render("faculty/charinaMoore");
+};
 
 module.exports.sylviaBorde = (req, res) => {
-    res.render('faculty/sylviaBorde')
-}
+  res.render("faculty/sylviaBorde");
+};
 
 module.exports.bettyMason = (req, res) => {
-    res.render('faculty/bettyMason')
-}
+  res.render("faculty/bettyMason");
+};
 
 module.exports.ceciliaPrior = (req, res) => {
-    res.render('faculty/ceciliaPrior')
-}
+  res.render("faculty/ceciliaPrior");
+};
 
 module.exports.linaFoltz = (req, res) => {
-    res.render('faculty/linaFoltz')
-}
+  res.render("faculty/linaFoltz");
+};
 
 //1ST GRADE
 module.exports.alexisJue = (req, res) => {
-    res.render('faculty/alexisJue')
-}
+  res.render("faculty/alexisJue");
+};
 
 module.exports.emilyScouten = (req, res) => {
-    res.render('faculty/emilyScouten')
-}
+  res.render("faculty/emilyScouten");
+};
 
 module.exports.terraJean = (req, res) => {
-    res.render('faculty/terraJean')
-}
-
+  res.render("faculty/terraJean");
+};
 
 //2ND GRADE
 module.exports.tracyHerrera = (req, res) => {
-    res.render('faculty/tracyHerrera')
-}
+  res.render("faculty/tracyHerrera");
+};
 
 module.exports.ashleyLecair = (req, res) => {
-    res.render('faculty/ashleyLecair')
-}
+  res.render("faculty/ashleyLecair");
+};
 
 module.exports.heatherCochnauer = (req, res) => {
-    res.render('faculty/heatherCochnauer')
-}
+  res.render("faculty/heatherCochnauer");
+};
 
 module.exports.rachelleLongtin = (req, res) => {
-    res.render('faculty/rachelleLongtin')
-}
+  res.render("faculty/rachelleLongtin");
+};
 
 //3RD GRADE
 module.exports.pamelaLucido = (req, res) => {
-    res.render('faculty/pamelaLucido')
-}
+  res.render("faculty/pamelaLucido");
+};
 
 module.exports.noriSwanson = (req, res) => {
-    res.render('faculty/noriSwanson')
-}
+  res.render("faculty/noriSwanson");
+};
 
 module.exports.haleyMax = (req, res) => {
-    res.render('faculty/haleyMax')
-}
-
+  res.render("faculty/haleyMax");
+};
 
 //4TH GRADE
 module.exports.stephanieKent = (req, res) => {
-    res.render('faculty/stephanieKent')
-}
+  res.render("faculty/stephanieKent");
+};
 
-module.exports.jasonStone = (req, res) => {
-    res.render('faculty/jasonStone')
-}
+module.exports.ericIsaacson = (req, res) => {
+  res.render("faculty/ericIsaacson");
+};
 
 //5TH GRADE
 module.exports.brendaChapman = (req, res) => {
-    res.render('faculty/brendaChapman')
-}
+  res.render("faculty/brendaChapman");
+};
 
 module.exports.aureliusMorgan = (req, res) => {
-    res.render('faculty/aureliusMorgan')
-}
+  res.render("faculty/aureliusMorgan");
+};
 
 //JUNIOR HIGH
 module.exports.steveSouza = (req, res) => {
-    res.render('faculty/steveSouza')
-}
+  res.render("faculty/steveSouza");
+};
 
 module.exports.andyPrior = (req, res) => {
-    res.render('faculty/andyPrior')
-}
+  res.render("faculty/andyPrior");
+};
 
 module.exports.toddKeough = (req, res) => {
-    res.render('faculty/toddKeough')
-}
+  res.render("faculty/toddKeough");
+};
 
 module.exports.tirzahBurke = (req, res) => {
-    res.render('faculty/tirzahBurke')
-}
+  res.render("faculty/tirzahBurke");
+};
 
 module.exports.joeyPrior = (req, res) => {
-    res.render('faculty/joeyPrior')
-}
+  res.render("faculty/joeyPrior");
+};
 
 //PHYSICAL EDUCATION
 module.exports.daranFraser = (req, res) => {
-    res.render('faculty/daranFraser')
-}
+  res.render("faculty/daranFraser");
+};
 
 module.exports.alexanderBuckley = (req, res) => {
-    res.render('faculty/alexanderBuckley')
-}
+  res.render("faculty/alexanderBuckley");
+};
 
 module.exports.coryBrady = (req, res) => {
-    res.render('faculty/coryBrady')
-}
+  res.render("faculty/coryBrady");
+};
 
 module.exports.amieRobison = (req, res) => {
-    res.render('faculty/amieRobison')
-}
+  res.render("faculty/amieRobison");
+};
 
-//ELECTIVES 
+//ELECTIVES
 module.exports.judyBurmeister = (req, res) => {
-    res.render('faculty/judyBurmeister')
-}
+  res.render("faculty/judyBurmeister");
+};
 
 module.exports.rebeccaKelly = (req, res) => {
-    res.render('faculty/rebeccaKelly')
-}
+  res.render("faculty/rebeccaKelly");
+};
 
 module.exports.kimberlynSchmitz = (req, res) => {
-    res.render('faculty/kimberlynSchmitz')
-}
+  res.render("faculty/kimberlynSchmitz");
+};
 
 module.exports.kristenMcDermott = (req, res) => {
-    res.render('faculty/kristenMcDermott')
-}
+  res.render("faculty/kristenMcDermott");
+};
 
 module.exports.margaretTrice = (req, res) => {
-    res.render('faculty/margaretTrice')
-}
+  res.render("faculty/margaretTrice");
+};
 
 //ADMINISTRATION
 module.exports.vernTaylor = (req, res) => {
-    res.render('faculty/vernTaylor')
-}
+  res.render("faculty/vernTaylor");
+};
 
 module.exports.theresaKukla = (req, res) => {
-    res.render('faculty/theresaKukla')
-}
+  res.render("faculty/theresaKukla");
+};
 
 module.exports.davePereira = (req, res) => {
-    res.render('faculty/davePereira')
-}
+  res.render("faculty/davePereira");
+};
 
 module.exports.rebecaShreve = (req, res) => {
-    res.render('faculty/rebecaShreve')
-}
+  res.render("faculty/rebecaShreve");
+};
 
 module.exports.pattiChan = (req, res) => {
-    res.render('faculty/pattiChan')
-}
+  res.render("faculty/pattiChan");
+};
 
 module.exports.lisaMazzoncini = (req, res) => {
-    res.render('faculty/lisaMazzoncini')
-}
+  res.render("faculty/lisaMazzoncini");
+};
 
 module.exports.lauriePease = (req, res) => {
-    res.render('faculty/lauriePease')
-}
+  res.render("faculty/lauriePease");
+};
 
 module.exports.cathyColomb = (req, res) => {
-    res.render('faculty/cathyColomb')
-}
+  res.render("faculty/cathyColomb");
+};
--- routes/faculty.js
+++ routes/faculty.js
@@ -1,109 +1,97 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const faculty = require('../controllers/faculty');
-
-
+const faculty = require("../controllers/faculty");
 
 //TK & KINDERGARTEN
-router.get('/charinaMoore', faculty.charinaMoore)
+router.get("/charinaMoore", faculty.charinaMoore);
 
-router.get('/sylviaBorde', faculty.sylviaBorde)
+router.get("/sylviaBorde", faculty.sylviaBorde);
 
-router.get('/bettyMason', faculty.bettyMason)
+router.get("/bettyMason", faculty.bettyMason);
 
-router.get('/ceciliaPrior', faculty.ceciliaPrior)
+router.get("/ceciliaPrior", faculty.ceciliaPrior);
 
-router.get('/linaFoltz', faculty.linaFoltz)
-
+router.get("/linaFoltz", faculty.linaFoltz);
 
 //1ST GRADE
-router.get('/alexisJue', faculty.alexisJue)
+router.get("/alexisJue", faculty.alexisJue);
 
-router.get('/emilyScouten', faculty.emilyScouten)
+router.get("/emilyScouten", faculty.emilyScouten);
 
-router.get('/terraJean', faculty.terraJean)
-
+router.get("/terraJean", faculty.terraJean);
 
 //2ND GRADE
-router.get('/tracyHerrera', faculty.tracyHerrera)
+router.get("/tracyHerrera", faculty.tracyHerrera);
 
-router.get('/ashleyLecair', faculty.ashleyLecair)
+router.get("/ashleyLecair", faculty.ashleyLecair);
 
-router.get('/heatherCochnauer', faculty.heatherCochnauer)
+router.get("/heatherCochnauer", faculty.heatherCochnauer);
 
-router.get('/rachelleLongtin', faculty.rachelleLongtin)
-
+router.get("/rachelleLongtin", faculty.rachelleLongtin);
 
 //3RD GRADE
-router.get('/pamelaLucido', faculty.pamelaLucido)
+router.get("/pamelaLucido", faculty.pamelaLucido);
 
-router.get('/noriSwanson', faculty.noriSwanson)
+router.get("/noriSwanson", faculty.noriSwanson);
 
-router.get('/haleyMax', faculty.haleyMax)
-
+router.get("/haleyMax", faculty.haleyMax);
 
 //4TH GRADE
-router.get('/stephanieKent', faculty.stephanieKent)
+router.get("/stephanieKent", faculty.stephanieKent);
 
-router.get('/jasonStone', faculty.jasonStone)
-
+router.get("/ericIsaacson", faculty.ericIsaacson);
 
 //5TH GRADE
-router.get('/brendaChapman', faculty.brendaChapman)
+router.get("/brendaChapman", faculty.brendaChapman);
 
-router.get('/aureliusMorgan', faculty.aureliusMorgan)
-
+router.get("/aureliusMorgan", faculty.aureliusMorgan);
 
 //JUNIOR HIGH
-router.get('/steveSouza', faculty.steveSouza)
+router.get("/steveSouza", faculty.steveSouza);
 
-router.get('/andyPrior', faculty.andyPrior)
+router.get("/andyPrior", faculty.andyPrior);
 
-router.get('/toddKeough', faculty.toddKeough)
+router.get("/toddKeough", faculty.toddKeough);
 
-router.get('/tirzahBurke', faculty.tirzahBurke)
+router.get("/tirzahBurke", faculty.tirzahBurke);
 
-router.get('/joeyPrior', faculty.joeyPrior)
-
+router.get("/joeyPrior", faculty.joeyPrior);
 
 //PHYSICAL EDUCATION
-router.get('/daranFraser', faculty.daranFraser)
+router.get("/daranFraser", faculty.daranFraser);
 
-router.get('/alexanderBuckley', faculty.alexanderBuckley)
+router.get("/alexanderBuckley", faculty.alexanderBuckley);
 
-router.get('/coryBrady', faculty.coryBrady)
+router.get("/coryBrady", faculty.coryBrady);
 
-router.get('/amieRobison', faculty.amieRobison)
-
+router.get("/amieRobison", faculty.amieRobison);
 
 //ELECTIVES
-router.get('/judyBurmeister', faculty.judyBurmeister)
+router.get("/judyBurmeister", faculty.judyBurmeister);
 
-router.get('/rebeccaKelly', faculty.rebeccaKelly)
+router.get("/rebeccaKelly", faculty.rebeccaKelly);
 
-router.get('/kimberlynSchmitz', faculty.kimberlynSchmitz)
+router.get("/kimberlynSchmitz", faculty.kimberlynSchmitz);
 
-router.get('/kristenMcDermott', faculty.kristenMcDermott)
+router.get("/kristenMcDermott", faculty.kristenMcDermott);
 
-router.get('/margaretTrice', faculty.margaretTrice)
-
+router.get("/margaretTrice", faculty.margaretTrice);
 
 //ADMINISTRATION
-router.get('/vernTaylor', faculty.vernTaylor)
+router.get("/vernTaylor", faculty.vernTaylor);
 
-router.get('/theresaKukla', faculty.theresaKukla)
+router.get("/theresaKukla", faculty.theresaKukla);
 
-router.get('/davePereira', faculty.davePereira)
+router.get("/davePereira", faculty.davePereira);
 
-router.get('/rebecaShreve', faculty.rebecaShreve)
+router.get("/rebecaShreve", faculty.rebecaShreve);
 
-router.get('/pattiChan', faculty.pattiChan)
+router.get("/pattiChan", faculty.pattiChan);
 
-router.get('/lisaMazzoncini', faculty.lisaMazzoncini)
+router.get("/lisaMazzoncini", faculty.lisaMazzoncini);
 
-router.get('/lauriePease', faculty.lauriePease)
+router.get("/lauriePease", faculty.lauriePease);
 
-router.get('/cathyColomb', faculty.cathyColomb)
-
+router.get("/cathyColomb", faculty.cathyColomb);
 
 module.exports = router;
