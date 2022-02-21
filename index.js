51430b8368427ddb5f7837e410f54dbdc5aa9ed6
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-02-20 17:58:45 -0800

    update tuition and fees

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const homeRoutes = require('./routes/home');
-const familyRoutes = require('./routes/family');
-const facultyRoutes = require('./routes/faculty');
+const homeRoutes = require("./routes/home");
+const familyRoutes = require("./routes/family");
+const facultyRoutes = require("./routes/faculty");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', homeRoutes)
-app.use('/family', familyRoutes)
-app.use('/faculty', facultyRoutes)
-
-
+app.use("/", homeRoutes);
+app.use("/family", familyRoutes);
+app.use("/faculty", facultyRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
-    console.log(`serving on port ${port}`)
-})
+  console.log(`serving on port ${port}`);
+});
